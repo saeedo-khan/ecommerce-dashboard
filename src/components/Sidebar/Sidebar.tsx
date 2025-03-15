a56f46467da641ec44edb7107858a80fddc64553
Saeedo <66878262+saeedo-khan@users.noreply.github.com>
2025-03-15 20:42:22 +0300

    init

---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className='max-w-[296px] w-full bg-primary'>
+        <div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
